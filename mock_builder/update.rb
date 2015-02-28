d9bb83bddb4474125b664a7cce3c1d3b72a4c0a4
Steven Spriggs <steven.spriggs@gmail.com>
2015-02-27 23:09:11 -0500

    clean up the update command

--- mock_builder/update.rb
+++ mock_builder/update.rb
@@ -5,8 +5,24 @@ class Update < Vagrant.plugin(2, :command)
   end
 
   def execute
-    puts "Updating Mock Builder..."
-    `vagrant ssh -c "sudo kill -9 $(ps ax | grep '[m]ock_server.rb' | awk '{print $1}') && cd /srv/mock_builder && sudo git pull && bundle install && cd mock_builder && ruby mock_server.rb --daemon 1" default`
-    puts "Mock Builder updated!"
+
+    command1 = "sudo kill -9 $(ps ax | grep '[m]ock_server.rb' | awk '{print $1}')"
+    command2 = "cd /srv/mock_builder && sudo git pull"
+    command3 = "cd /srv/mock_builder && sudo bundle install"
+    command4 = "cd /srv/mock_builder/mock_builder && ruby mock_server.rb --daemon 1"
+
+    with_target_vms(nil, :single_target => true) do |vm|
+      safe_puts("Stopping Mock Builder Processes...")
+      vm.action(:ssh_run, :ssh_run_command => command1, ssh_opts: {extra_args: ["-qT"]})
+      safe_puts("Pulling latest code from GitHub...")
+      vm.action(:ssh_run, :ssh_run_command => command2, ssh_opts: {extra_args: ["-qT"]})
+      safe_puts("Running Bundle Install...")
+      vm.action(:ssh_run, :ssh_run_command => command3, ssh_opts: {extra_args: ["-qT"]})
+      safe_puts("Restarting Mock Builder...")
+      vm.action(:ssh_run, :ssh_run_command => command4, ssh_opts: {extra_args: ["-qT"]})
+      safe_puts("Finished Upgrade!")
+      return 0
+    end
+
   end
 end
