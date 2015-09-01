905ba647bc89cfe7abc16757597fc9033fb30a0f
Steven Spriggs <steven.spriggs@gmail.com>
2015-09-01 15:01:37 -0400

    do not load mock builder in daemon mode, this causes bathing to load incorrectly.

--- mock_builder/update.rb
+++ mock_builder/update.rb
@@ -23,7 +23,7 @@ module MockBuilder
       command1 = "sudo kill -9 $(ps ax | grep '[m]ock_server.rb' | awk '{print $1}')"
       command2 = "cd /srv/mock_builder && sudo git pull"
       command3 = "cd /srv/mock_builder && sudo bundle install"
-      command4 = "cd /srv/mock_builder/mock_builder && ruby mock_server.rb --daemon 1"
+      command4 = "cd /srv/mock_builder/mock_builder && ruby mock_server.rb --daemon 0"
 
       with_target_vms(nil, :single_target => true) do |vm|
         safe_puts("Stopping Mock Builder Processes...")
